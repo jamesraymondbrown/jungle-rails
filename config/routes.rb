e8818f98e2a7263c2382f40f384027c6af41705e
James Brown <jamesraymondbrown@gmail.com>
2023-01-26 18:33:17 +0000

    add files for new admin/categories section

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-
+  
   get 'about/index'
   root to: 'products#index'
 
@@ -16,6 +16,7 @@ Rails.application.routes.draw do
   namespace :admin do
     root to: 'dashboard#show'
     resources :products, except: [:edit, :update, :show]
+    resources :categories, except: [:edit, :update, :show]
   end
 
   # The priority is based upon order of creation: first created -> highest priority.
