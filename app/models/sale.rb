b386cf285cb9713321016a9f4456a28cb71a6e34
James Brown <jamesraymondbrown@gmail.com>
2023-01-27 03:37:41 +0000

    add sale banner to application view

--- app/models/sale.rb
+++ app/models/sale.rb
@@ -1,2 +1,20 @@
 class Sale < ApplicationRecord
+
+  def self.active
+    self.where("sales.starts_on <= ? AND sales.ends_on >= ?", Date.current, Date.current).any?
+  end
+
+  def finished?
+    # The self. here is unnecessary (as seen in future methods), but I've kept it for my future reference
+    self.ends_on < Date.current
+  end
+
+  def upcoming?
+    starts_on > Date.current
+  end
+
+  def active?
+    !upcoming? && !finished?
+  end
+
 end
