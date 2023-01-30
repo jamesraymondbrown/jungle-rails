e1c39e527afc359af0fc23244ea46682fbcdc7ca
James Brown <jamesraymondbrown@gmail.com>
2023-01-30 21:48:06 +0000

    add test to validate that user has a name

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,4 +2,7 @@ class User < ApplicationRecord
   
   has_secure_password
 
+  validates :email, presence: true
+  validates :name, presence: true
+
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,15 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  
+  describe 'Validations' do
+    it 'should validate that the user has a name' do
+      @user = User.new(email: 'james@james.com')
+      @user.save
+
+      expect(@user.errors.full_messages).to include("Name can't be blank")
+    end
+
+  end
+
 end
