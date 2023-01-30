d610b6ff0e6460b71271aa476d2c252c9d0aaf42
James Brown <jamesraymondbrown@gmail.com>
2023-01-30 22:00:23 +0000

    add test to validate that password and password confirmation fields aren't blank

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -3,11 +3,25 @@ require 'rails_helper'
 RSpec.describe User, type: :model do
   
   describe 'Validations' do
-    it 'should validate that the user has a name' do
-      @user = User.new(email: 'james@james.com')
+    it 'should validate that the user has a name and email' do
+      @user = User.new()
       @user.save
 
-      expect(@user.errors.full_messages).to include("Name can't be blank")
+      expect(@user.errors.full_messages).to include("Name can't be blank", "Email can't be blank")
+    end
+
+    it 'should validate that the password and password confirmation are required' do
+      @user = User.new()
+      @user.save
+
+      expect(@user.errors.full_messages).to include("Password can't be blank", "Password confirmation can't be blank")
+    end
+
+    it 'should validate that the password and password confirmation match' do
+      @user = User.new()
+      @user.save
+
+      expect(@user.errors.full_messages).to include("Password can't be blank", "Password confirmation can't be blank")
     end
 
   end
