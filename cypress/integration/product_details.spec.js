26b8498999b7fa96b5700de4c4c9477774a95e7a
James Brown <jamesraymondbrown@gmail.com>
2023-01-31 22:05:00 +0000

    add product-details test

---
+++ cypress/integration/product_details.spec.js
@@ -0,0 +1,29 @@
+context('Product Page', () => {
+  beforeEach(() => {
+  })
+
+  it('visits the home page', () => {
+    // https://on.cypress.io/type
+    cy.visit('/', { timeout: 500000 })
+    cy.get('nav').should('exist')
+  })
+
+  it("There is products on the page", () => {
+    cy.get(".products article").should("be.visible");
+  });
+
+  it("There are 2 products on the page", () => {
+    cy.get(".products article").should("have.length", 2);
+  });
+
+  it("clicks on a product", () => {
+    cy.contains("Scented Blade").click();
+  });
+
+  it('navigates to a new page when the product is clicked', () => {
+    cy.contains("The Scented Blade is an extremely rare")
+  })
+
+
+
+})
